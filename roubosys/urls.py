8e27d495019c772c5996b7f792caea4aef49ca5e
JunJian <junjian@mochui.net>
2015-11-07 19:37:36 +0800

    modify views

--- roubosys/urls.py
+++ roubosys/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from roubosys.views import hello bchat_outgoing
+from roubosys.views import hello, bchat_outgoing
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
