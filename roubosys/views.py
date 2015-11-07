4fc924e547559ed2b7ce18287fa4c23ebf0fdd4b
JunJian <junjian@mochui.net>
2015-11-07 23:28:59 +0800

    fix

--- roubosys/views.py
+++ roubosys/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 def hello(request):
   return HttpResponse("Hello rouboSyS")
 
-def bchat_outgoing(request):
-  return HttpResponse("Get a message")
+def bchat_outgoing(request, outstring):
+  return HttpResponse("Get a message" + outstring)
