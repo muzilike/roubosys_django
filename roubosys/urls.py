ac9337bfd2a335dcfb1cc337a4f3e377d586453b
JunJian <junjian@mochui.net>
2015-11-07 10:10:55 +0800

    add the views and url conf

--- roubosys/urls.py
+++ roubosys/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from roubosys.views import hello
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello),
 ]
