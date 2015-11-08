f8aa0c8ecdf67b49e2b78af1666fc1c297d02f57
JunJian <junjian@mochui.net>
2015-11-08 22:59:53 +0800

    add mp2.5

--- roubosys/daily_use/weather.py
+++ roubosys/daily_use/weather.py
@@ -4,10 +4,11 @@ import json
 
 def now():
     res_text = ""
-    url = "http://apis.baidu.com/apistore/weatherservice/weather?citypinyin=shanghai"
+    url_weather = "http://apis.baidu.com/apistore/weatherservice/weather?citypinyin=shanghai"
+    url_pm25 = "http://apis.baidu.com/apistore/aqiservice/aqi?city=%e4%b8%8a%e6%b5%b7"
     apikey = "f90d577c266f1869340ae74e1c409669"
     headers = { 'apikey' : apikey}
-    req = urllib2.Request(url = url, headers = headers)
+    req = urllib2.Request(url = url_weather, headers = headers)
     f = urllib2.urlopen(req)
     res = json.loads(f.read())
     if res["errMsg"] == "success":
@@ -17,4 +18,15 @@ def now():
         res_text = res_text + u"温度范围: " + res["retData"]["l_tmp"] + "~" + res["retData"]["h_tmp"] + "\n"
     else:
         res_text = "Get weather failed: " + res["errMsg"]
+
+    req = urllib2.Request(url = url_pm25, headers = headers)
+    f = urllib2.urlopen(req)
+    res = json.loads(f.read())
+    if res["errMsg"] == "success":
+        res_text = res_text + u"空气指数:" + res["retData"]["aqi"] + "\n"
+        res_text = res_text + u"空气水平: " + res["retData"]["level"] + "\n"
+        res_text = res_text + u"污染物类型: " + res["retData"]["core"] + "\n"
+    else:
+        res_text = res_text + "Get pm25 failed: " + res["errMsg"]
+
     return res_text
